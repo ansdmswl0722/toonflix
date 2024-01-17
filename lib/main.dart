450b2941211b75d0b9b26dc46e34a765aa290a78
ansdmswl0722 <ansdmswl0722@gmail.com>
2024-01-17 17:08:03 +0900

    위젯 변경 완료

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,15 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
